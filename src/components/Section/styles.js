5e7316d9cc938c77f680cdcb43f0d17f95c223dd
pcaldi <pcaldi@gmail.com>
2024-08-21 11:51:34 -0300

    component section

---
+++ src/components/Section/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+
+> h2 {
+
+  margin-bottom: 16px;
+
+  color: ${({ theme }) => theme.COLORS.GRAY_100};
+  font-family: 'Roboto';
+  font-size: 16px;
+  font-weight: 400;
+}
+
+`;
