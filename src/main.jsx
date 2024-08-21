f90ccef1112f84bae492ca1200ec71fc76e3c637
pcaldi <pcaldi@gmail.com>
2024-08-21 16:18:25 -0300

    page dish

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import { ThemeProvider } from 'styled-components'
 import theme from "./styles/theme"
 import GlobalStyles from "./styles/global"
 
-import { New } from './pages/New'
+import { Dish } from './pages/Dish'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <New />
+      <Dish />
     </ThemeProvider>
   </StrictMode>,
 )
---
+++ src/pages/Dish/index.jsx
@@ -0,0 +1,17 @@
+import { Container } from "./styles";;
+
+import { Header } from "../../components/Header";
+import { Footer } from "../../components/Footer";
+
+export function Dish() {
+  return (
+    <Container>
+      <Header />
+
+
+
+      <Footer />
+
+    </Container>
+  )
+}
