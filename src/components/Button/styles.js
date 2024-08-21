cae11fd1f79f2a39f79ba01c0a7a9aefc6c58208
pcaldi <pcaldi@gmail.com>
2024-08-21 10:43:50 -0300

    component button

---
+++ src/components/Button/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+
+  width: 100%;
+  background: ${({ theme }) => theme.COLORS.DARK_RED};
+  color: ${({ theme }) => theme.COLORS.WHITE};
+
+  border: none;
+  border-radius: 5px;
+  padding: 12px 32px;
+
+  font-family: 'Poppins', sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 24px;
+
+  &:disabled {
+    opacity: 0.5;
+  }
+
+`;
