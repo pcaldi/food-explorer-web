99d7ea7959609250179fee403acbe6b358455c63
pcaldi <pcaldi@gmail.com>
2024-08-21 09:45:12 -0300

    definindo theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import { ThemeProvider } from 'styled-components'
+import theme from "./styles/theme"
+import GlobalStyles from "./styles/global"
+
+import { SignIn } from './pages/SignIn'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <SignIn />
+    </ThemeProvider>
   </StrictMode>,
 )
