b4bcb755d90e630a6a7f4bbe996591b8f0bca722
pcaldi <pcaldi@gmail.com>
2024-08-21 10:44:50 -0300

    page signIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,15 @@
 import { Container } from "./styles";
 
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+
 export function SignIn() {
   return (
     <Container>
-      SingIn
+      <Input placeholder="Exemplo: Maria da Silva" />
+      <Input placeholder="Exemplo: exemplo@exemplo.com.br" />
+      <Input placeholder="No mínimo 6 caracteres" />
+      <Button title="Entrar" />
     </Container>
   )
 }
