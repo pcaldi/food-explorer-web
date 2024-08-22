a1defb87febaa78296955f97280e8228275894c3
pcaldi <pcaldi@gmail.com>
2024-08-22 11:41:15 -0300

    page new

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,31 @@
-import { Header } from "../../components/Header";
-import { Container } from "./styles";
+import { Container, Item } from "./styles";
 
+import { FoodItem } from "../../components/FoodItem";
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+import { Textarea } from "../../components/Textarea";
 
 export function New() {
   return (
     <Container>
-      <Header />
+      <Header isAdmin />
+
+
+      <Section title="Ingredientes">
+        <Item>
+          <FoodItem placeholder="Pão" />
+          <FoodItem isNew placeholder="Adicionar" />
+        </Item>
+
+      </Section>
+
+      <Section title="Descrição">
+        <Textarea placeholder="Fale brevemente sobre o prato, seus ingredientes e composição" />
+      </Section>
+
+      <Footer />
+
     </Container>
   )
 }
--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -1,3 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+
+export const Item = styled.div`
+  max-width: 837px;
+  height: 48px;
+
+  background: ${({ theme }) => theme.COLORS.BACKGROUND_300};
+
+  border-radius: 8px;
+  padding: 8px 4px;
+
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 16px;
+
+
+`;
