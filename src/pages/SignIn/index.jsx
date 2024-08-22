d7a28ec0c8e1ee3268ede804e420b2aba191a418
pcaldi <pcaldi@gmail.com>
2024-08-22 12:07:25 -0300

    page signIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,15 +1,39 @@
-import { Container } from "./styles";
+import { Container, Form, Logo } from "./styles";
 
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
+import { Section } from "../../components/Section";
 
+import logoImg from "../../assets/logo.svg"
 export function SignIn() {
   return (
     <Container>
-      <Input placeholder="Exemplo: Maria da Silva" />
-      <Input placeholder="Exemplo: exemplo@exemplo.com.br" />
-      <Input placeholder="No mínimo 6 caracteres" />
-      <Button title="Entrar" />
+      <Logo>
+        <img src={logoImg} alt="Logo da empresa" />
+      </Logo>
+
+      <Form>
+        <h2>Faça seu login</h2>
+
+        <Section title="Email">
+          <Input
+            type="email"
+            placeholder="Exemplo: Maria da Silva"
+          />
+        </Section>
+
+        <Section title="Senha">
+          <Input
+            type="password"
+            placeholder="No mínimo 6 caracteres"
+          />
+        </Section>
+
+        <Button title="Entrar" />
+
+        <a href="#">Criar uma conta</a>
+      </Form>
+
     </Container>
   )
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,3 +1,60 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+  padding: 142px 108px 142px 153px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+`;
+
+export const Logo = styled.div`
+  display: flex;
+
+  img {
+    width: 324px;
+    height: 48px;
+  }
+
+`;
+
+export const Form = styled.div`
+  min-width: 476px;
+  padding: 64px;
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+
+  background: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+
+  border-radius: 16px;
+
+  > h2 {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    text-align: center;
+
+    font-family: 'Poppins';
+    font-size: 32px;
+    font-weight: 500;
+    line-height: 140%;
+  }
+
+  input {
+    border: 1px solid ${({ theme }) => theme.COLORS.GRAY_200};
+    border-radius: 5px;
+  }
+
+  a {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    text-align: center;
+
+    font-family: 'Poppins';
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 24px;
+  }
+
+`;
