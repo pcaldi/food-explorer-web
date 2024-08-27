b08427b1a82d3ba83bc3da0cd677409d42d1670d
pcaldi <pcaldi@gmail.com>
2024-08-27 08:53:40 -0300

    finalizando page new

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,28 +1,98 @@
-import { Container, Item } from "./styles";
+import { useNavigate } from "react-router-dom";
+
+import { Container, Form, Items, Image, Category } from "./styles";
 
 import { FoodItem } from "../../components/FoodItem";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
 import { Textarea } from "../../components/Textarea";
+import { ButtonText } from "../../components/ButtonText";
+import { FiChevronDown, FiChevronLeft, FiUpload } from "react-icons/fi";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
 
 export function New() {
+
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate("/")
+  }
+
+
   return (
     <Container>
       <Header isAdmin />
+      <main>
+        <Form>
+          <header>
+            <ButtonText onClick={handleBack}>
+              <FiChevronLeft />
+              voltar
+            </ButtonText>
+
+            <h1>Adicionar prato</h1>
+          </header>
+
+          <Items>
+            <Section title="Imagem do prato">
+              <Image>
+                <label htmlFor="image">
+                  <FiUpload />
+                  <span>Selecione imagem</span>
+                </label>
+              </Image>
+
+            </Section>
+
+            <Section title="Nome">
+              <Input className="name" placeholder="Ex.: Salada Caesar" />
+            </Section>
+
+            <Section title="Categoria">
+              <Category>
+                <label htmlFor="category">
+                  <select id="category">
+                    <option value="meal">Refeição</option>
+                    <option value="drink">Bebidas</option>
+                    <option value="dessert">Sobremesa</option>
+                  </select>
+
+                  <FiChevronDown />
+                </label>
+              </Category>
+            </Section>
+          </Items>
+
+          <div className="ing-price">
+            <Section title="Ingredientes">
+              <div className="tags">
+                <FoodItem placeholder="Pão Naann" />
+                <FoodItem placeholder="Adicionar" isNew />
+              </div>
+            </Section>
+
+            <Section title="Preço">
+              <Input className="price" placeholder="R$ 00,00" />
+            </Section>
+          </div>
 
 
-      <Section title="Ingredientes">
-        <Item>
-          <FoodItem placeholder="Pão" />
-          <FoodItem isNew placeholder="Adicionar" />
-        </Item>
+          <Section title="Descrição">
+            <Textarea placeholder="Fale brevemente sobre o prato, seus ingredientes e composição" />
+          </Section>
 
-      </Section>
 
-      <Section title="Descrição">
-        <Textarea placeholder="Fale brevemente sobre o prato, seus ingredientes e composição" />
-      </Section>
+          <div className="btn">
+            <Button title="Salvar alterações" />
+          </div>
+
+
+        </Form>
+      </main>
+
+
 
       <Footer />
 
--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -1,20 +1,211 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
 
-export const Item = styled.div`
-  max-width: 837px;
-  height: 48px;
+  display: grid;
+  grid-template-rows: 9.6rem auto 7.7rem;
+  grid-template-areas:
+    "header"
+    "content"
+    "footer";
 
-  background: ${({ theme }) => theme.COLORS.BACKGROUND_300};
-
-  border-radius: 8px;
-  padding: 8px 4px;
-
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 16px;
-
+  > main {
+    grid-area: content;
+    overflow-y: auto;
+  }
 
 `;
+
+export const Form = styled.div`
+    display: flex;
+    width: 112.0rem;
+    flex-direction: column;
+    gap: 2.4rem;
+
+    margin: 0 auto;
+
+
+  > header {
+    display: flex;
+    flex-direction: column;
+
+     button {
+      margin-top: 20px;
+    }
+
+    h1 {
+      padding-top: 2.4rem;
+      font-family: "Poppins", sans-serif;
+      font-weight: 500;
+      line-height: 140%;
+      color: ${({ theme }) => theme.COLORS.GRAY_200};
+    }
+  }
+
+  .ing-price {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .tags {
+    width: 82.1rem;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    gap: .8rem;
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+
+    padding: .6rem .8rem;
+    border-radius: .8rem;
+
+    &:focus-within {
+      outline: .1rem solid ${({ theme }) => theme.COLORS.WHITE};
+    }
+
+    input {
+      background-color: transparent;
+    }
+  }
+
+  .price {
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+    border-radius: .8rem;
+    padding: 1.2rem 1.4rem;
+
+
+    &:focus-within {
+      outline: .1rem solid ${({ theme }) => theme.COLORS.WHITE};
+    }
+  }
+
+  .btn {
+    display: flex;
+    justify-content: flex-end;
+
+    button {
+      max-width: 17.2rem;
+      padding: 1.2rem 2.4rem;
+      background: ${({ theme }) => theme.COLORS.LIGHT_RED};
+    }
+
+  }
+
+`;
+
+export const Items = styled.div`
+  display: flex;
+  gap: 2.4rem;
+
+  section input  {
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+    border-radius: .8rem;
+  }
+
+  .name {
+    min-width: 46.3rem;
+  }
+
+`
+
+export const Image = styled.div`
+  padding: 1.2rem 3.2rem;
+  width: 100%;
+  min-width: 22.9rem;
+  border-radius: .8rem;
+  position: relative;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+  color: ${({ theme }) => theme.COLORS.WHITE};
+
+  &:focus-within {
+    outline: .1rem solid ${({ theme }) => theme.COLORS.WHITE};
+  }
+
+  > label {
+    display: flex;
+    gap: .8rem;
+    cursor: pointer;
+
+    svg {
+      width: 2.4rem;
+      height: 2.4rem;
+    }
+
+    span {
+      font-family: "Poppins", sans-serif;
+      font-size: 1.4rem;
+      line-height: 2.4rem;
+    }
+
+    input {
+      position: absolute;
+      right: 0;
+      z-index: -1;
+
+      max-width: 22.9rem;
+    }
+
+    svg, span {
+      transition: filter 0.2s;
+    }
+
+    &:hover {
+      svg, span {
+        filter: brightness(0.9);
+      }
+    }
+  }
+`
+
+export const Category = styled.div`
+  width: 100%;
+  min-width: 36.4rem;
+
+  > label {
+    position: relative;
+    width: 100%;
+    padding: 1.2rem 1.6rem;
+    border-radius: .8rem;
+
+
+    select {
+      border: none;
+      appearance: none;
+      cursor: pointer;
+
+      width: 100%;
+      padding: 1.2rem 1.6rem;
+      border-radius: .8rem;
+
+      background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+
+      font-weight: 400;
+      font-size: 1.4rem;
+      line-height: 160%;
+    }
+
+    svg {
+      width: 2.4rem;
+      height: 2.4rem;
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+      position: absolute;
+      top: 1.2rem;
+      right: -35rem;
+      cursor: pointer;
+      pointer-events: none;
+      transition: filter 0.2s;
+    }
+    &:hover {
+      svg {
+        filter: brightness(0.9);
+      }
+    }
+  }
+
+`;
+
+
