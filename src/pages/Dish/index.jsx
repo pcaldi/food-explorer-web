882e23e853e585ccb4a5f805f8a0e8adfa801973
pcaldi <pcaldi@gmail.com>
2024-08-27 08:54:00 -0300

    finalizando page dish

--- src/pages/Dish/index.jsx
+++ src/pages/Dish/index.jsx
@@ -1,29 +1,79 @@
+/* eslint-disable react/prop-types */
+import { useNavigate } from "react-router-dom";
+
 import { FiChevronLeft } from "react-icons/fi";
 
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
 
-import { ButtonText } from "../../components/ButtonText";
+import { Tag } from "../../components/Tag";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
-import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
 import { QuantityPicker } from "../../components/QuantityPicker";
 
-export function Dish() {
+import dishImg from "../../assets/salada_ravanello.png"
+import { Button } from "../../components/Button";
+
+export function Dish({ isAdmin }) {
+
+  const navigate = useNavigate()
+
+  function handleBack() {
+    navigate("/")
+  }
+
+  function handleNextPage() {
+    navigate("/edit")
+  }
+
+
   return (
     <Container>
-      <Header />
+      <Header isAdmin />
+      <main>
 
-      <ButtonText>
-        <FiChevronLeft size={32} />
-        voltar
-      </ButtonText>
+        <header>
+          <ButtonText onClick={handleBack}>
+            <FiChevronLeft size={32} />
+            voltar
+          </ButtonText>
+        </header>
 
-      <Tag title="Tomate" />
-      <Tag title="Alface" />
-      <Tag title="Cebola" />
+        <Content>
 
-      <QuantityPicker />
+          <img src={dishImg} alt="Imagem do prato" />
 
+          <div>
+            <h1>Salada Ravanello</h1>
+            <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim. O pão naan dá um toque especial.</p>
+
+            <section>
+              <Tag title="Alface" />
+              <Tag title="Cebola" />
+              <Tag title="Pão naan" />
+              <Tag title="Rabanete" />
+              <Tag title="Pepino" />
+              <Tag title="Tomate" />
+
+            </section>
+
+            {isAdmin ?
+              <div className="buttons">
+
+                <QuantityPicker />
+                <Button title="incluir" />
+              </div>
+              :
+              <div className="buttons">
+                <Button title="Editar" onClick={handleNextPage} />
+              </div>
+            }
+
+          </div>
+
+        </Content>
+
+      </main>
 
       <Footer />
 
--- src/pages/Dish/styles.js
+++ src/pages/Dish/styles.js
@@ -1,3 +1,65 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 9.6rem auto 7.7rem;
+  grid-template-areas:
+    "header"
+    "content"
+    "footer";
+
+  > main {
+    grid-area: content;
+    overflow-y: auto;
+    max-width: 112.4rem;
+    margin: 3.2rem auto 15.5rem;
+  }
+
+`;
+
+export const Content = styled.div`
+    display: flex;
+    gap: 4.7rem;
+    margin-top: 4.2rem;
+
+    img {
+      width: 39rem;
+      height: 39rem;
+    }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 2.4rem;
+
+    h1 {
+      font-family: "Poppins", sans-serif;
+      font-weight: 500;
+      font-size: 4.0rem;
+      line-height: 140%;
+      color: ${({ theme }) => theme.COLORS.GRAY_200};
+    }
+
+    p {
+      font-family: "Poppins", sans-serif;
+      font-weight: 400;
+      font-size: 2.4rem;
+      line-height: 140%;
+      color: ${({ theme }) => theme.COLORS.GRAY_200};
+    }
+  }
+  .buttons {
+    display: flex;
+    gap: 3.3rem;
+    margin-top: 5.2rem;
+
+    button {
+      width: 11.4rem;
+    }
+  }
+`;
+
