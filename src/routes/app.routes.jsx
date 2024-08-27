a8ad25898602bc56feb235ebcecc6ce4af36a257
pcaldi <pcaldi@gmail.com>
2024-08-27 08:54:45 -0300

    adicionando rota edit

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom"
 import { Dish } from "../pages/Dish"
 import { Home } from "../pages/Home"
 import { New } from "../pages/New"
+import { Edit } from "../pages/Edit"
 
 export function AppRoutes() {
   return (
@@ -10,6 +11,7 @@ export function AppRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/new" element={<New />} />
       <Route path="/dish/:id" element={<Dish />} />
+      <Route path="/edit" element={<Edit />} />
     </Routes>
   )
 }
