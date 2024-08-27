f507aa310b3d9244e5251f9eb0f971cea2959a70
pcaldi <pcaldi@gmail.com>
2024-08-27 08:52:15 -0300

    styles footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINT } from "../../styles/deviceBreakPoints";
 
 export const Container = styled.footer`
   grid-area: footer;
@@ -11,16 +12,27 @@ export const Container = styled.footer`
 
   width: 100%;
   height: 7.7rem;
-  padding: 2.4rem 12.3rem;
+
+  padding: 2.9rem 2.8rem;
   align-items: center;
 
   background: ${({ theme }) => theme.COLORS.BACKGROUND_300};
 
+  @media (min-width: ${DEVICE_BREAKPOINT.LG}){
+    padding: 2.4rem 12.3rem;
+  }
+
 `;
 
 export const Brand = styled.image`
-    width: 18.6rem;
-    height: 3.0rem;
+      width: 12rem;
+      height: 1.8rem;
+
+    @media (min-width: ${DEVICE_BREAKPOINT.LG}){
+        width: 18.6rem;
+        height: 3.0rem;
+
+    }
 `;
 
 export const Copyright = styled.div`
@@ -28,9 +40,13 @@ export const Copyright = styled.div`
 > span {
   color: ${({ theme }) => theme.COLORS.WHITE};
 
-  font-size: 1.4rem;
+  font-size: 1.2rem;
   font-weight: 400;
   line-height: 160%;
+
+  @media (min-width: ${DEVICE_BREAKPOINT.LG}){
+    font-size: 1.4rem;
+    }
 }
 
 
