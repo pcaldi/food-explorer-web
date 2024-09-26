a6e0799ca311a5f983a36c0905f66415499cd334
pcaldi <pcaldi@gmail.com>
2024-09-26 11:03:11 -0300

    home finalizada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,54 +1,132 @@
-import { Container, Dishes } from "../HomeOriginal/styles";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Container, Dishes } from "./styles";
 
 import { Footer } from "../../components/Footer";
-import { Header } from "../../components/Header";
-import { FoodCard } from "../../components/FoodCard";
 import { Slider } from "../../components/Slider";
-import { Section } from "../../components/Section";
+import { Header } from "../../components/Header";
 import { Banner } from "../../components/Banner";
+import { Section } from "../../components/Section";
+import { FoodCard } from "../../components/FoodCard";
+
+import { api } from "../../services/api";
 
 export function Home() {
 
-  const data = {
-    "title": "Salada Ravanello",
-    "description": "Uma salada com carne seca, cenoura, tomate, alface, azeitona e orégano.",
-    "price": "48.90"
+  const [search, setSearch] = useState("");
+  const [dishes, setDishes] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+
+  const navigate = useNavigate();
+
+
+  function handleDishDetail(id) {
+    navigate(`/dish/${id}`);
   }
+
+
+  useEffect(() => {
+    async function fetchDishes() {
+
+      const response = await api.get(`/dishes?name=${search}`);
+      setDishes(response.data);
+    }
+
+    fetchDishes();
+  }, [search]);
+
+  useEffect(() => {
+    async function fetchFavorites() {
+      try {
+        const response = await api.get("/favorites");
+        const favorites = response.data.map((favorite) => favorite.dish_id);
+        setFavorites(favorites);
+      } catch (error) {
+        console.log("Erro ao buscar favoritos:", error);
+      }
+    };
+    fetchFavorites();
+  }, []);
+
+  async function updateFavorite(isFavorite, dishId) {
+    try {
+      if (isFavorite) {
+        await api.delete(`/favorites/${dishId}`);
+        setFavorites((prevFavorites) =>
+          prevFavorites.filter((favorite) => favorite !== dishId)
+        );
+      } else {
+        await api.post('/favorites', { dish_id: dishId });
+        setFavorites((prevFavorites) => [...prevFavorites, dishId]);
+      }
+    } catch (error) {
+      console.log('Erro ao atualizar favoritos:', error);
+    }
+  };
 
   return (
     <Container>
-      <Header />
+      <Header setSearch={setSearch} />
 
       <main>
         <Banner />
+
         <Dishes>
-          <Section title="Refeição">
-            <Slider>
-              <FoodCard data={data} />
-              <FoodCard data={data} />
-              <FoodCard data={data} />
-              <FoodCard data={data} />
+          <Section name="Refeição">
+            <Slider >
+              {
+                dishes.filter(dish => dish.category == 'meal')
+                  .map((dish) => (
+                    <FoodCard
+                      key={String(dish.id)}
+                      data={dish}
+                      onClick={() => handleDishDetail(dish.id)}
+                      isFavorite={favorites.includes(dish.id)}
+                      updateFavorite={updateFavorite}
+                    />
+                  ))
+              }
             </Slider>
           </Section>
 
-          <Section title="Bebidas">
-            <Slider>
-              <FoodCard data={data} />
-              <FoodCard data={data} />
-              <FoodCard data={data} />
-              <FoodCard data={data} />
+          <Section name="Bebidas">
+            <Slider >
+              {
+                dishes.filter(dish => dish.category == 'drink')
+                  .map((dish) => (
+                    <FoodCard
+                      key={String(dish.id)}
+                      data={dish}
+                      onClick={() => handleDishDetail(dish.id)}
+                      isFavorite={favorites.includes(dish.id)}
+                      updateFavorite={updateFavorite}
+                    />
+                  ))
+              }
             </Slider>
           </Section>
 
-          <Section title="Refeição">
+          <Section name="Sobremesas">
             <Slider>
-              <FoodCard data={data} />
-              <FoodCard data={data} />
-              <FoodCard data={data} />
-              <FoodCard data={data} />
+              {
+                dishes.filter(dish => dish.category == 'dessert')
+                  .map((dish) => (
+                    <FoodCard
+                      key={String(dish.id)}
+                      data={dish}
+                      onClick={() => handleDishDetail(dish.id)}
+                      isFavorite={favorites.includes(dish.id)}
+                      updateFavorite={updateFavorite}
+                    />
+                  ))
+              }
             </Slider>
           </Section>
         </Dishes>
+
+
+
       </main>
 
       <Footer />
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,56 +1,33 @@
 import styled from "styled-components";
-import { DEVICE_BREAKPOINT } from "../../styles/deviceBreakPoints";
 
 export const Container = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  display: grid;
-  grid-template-rows: auto 1fr auto;
+  width: 100% ;
 
+  display: grid;
+  grid-template-rows: auto;
   grid-template-areas:
     "header"
     "content"
     "footer";
 
   > main {
-      grid-area: content;
-      max-width: 100vw;
-      min-height: 74.2vh;
-      overflow-x: hidden;
-    }
-
-  @media (max-width: ${DEVICE_BREAKPOINT.LG}) {
-    grid-template-rows: auto 1fr auto;
+    grid-area: content;
   }
+
 `;
 
-
 export const Dishes = styled.div`
+  max-width: 112.2rem;
+  margin: 0 auto;
   padding: 2.4rem;
   display: flex;
   flex-direction: column;
-  max-width: 100vw;
+  margin-top: 2rem;
 
-  > section h2 {
-    margin-bottom: 1.6rem;
+ > section h2 {
+    margin-bottom: 0;
     font-size: 2.4rem;
-  }
-
-  @media (max-width: ${DEVICE_BREAKPOINT.LG}) {
-    padding: 1.6rem;
-
-    > section h2 {
-      font-size: 2rem;
-    }
-  }
-
-  @media (max-width: ${DEVICE_BREAKPOINT.MD}) {
-    padding: 1.2rem;
-
-    > section h2 {
-      font-size: 1.8rem;
-    }
-  }
+ }
 `;
 
 
