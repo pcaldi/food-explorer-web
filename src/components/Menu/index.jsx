1a5221b41084ee3600dc97f2f7eb285eaca9e62e
pcaldi <pcaldi@gmail.com>
2024-10-03 16:56:12 -0300

    component menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -5,11 +5,19 @@ import { Header } from "../Header";
 import { Search } from "../Search";
 import { ButtonText } from "../ButtonText";
 
+import { USER_ROLES } from "../../utils/roles";
 
-export function Menu({ isAdmin, isMenuOpen, setIsMenuOpen }) {
+import { useAuth } from "../../hooks/auth";
+
+export function Menu({ isMenuOpen, setIsMenuOpen }) {
+
+  const { user } = useAuth();
+  const isAdmin = [USER_ROLES.ADMIN].includes(user.role);
+
+
   return (
     <Container>
-      <Header isAdmin={isAdmin} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <main>
         <Search />
 
